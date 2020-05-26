8c41b4c4e5942099428954ecafcb017b5412ca40
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2020-05-26 16:38:39 -0400

    Implement client side JS to fetch API

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,11 @@
 console.log('script loaded')
+
+fetch('/weather?address="Las Terrenas"').then(response => {
+  response.json().then(data => {
+    if (data.error) {
+      console.error(data.error)
+    } else {
+      console.log(data)
+    }
+  })
+})
